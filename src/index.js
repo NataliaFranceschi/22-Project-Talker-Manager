81096596b34fd713da3529276c7ee3c7d9097e70
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-11-06 10:55:07 -0300

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkerManager = require('./talkerManager');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (req, res) => {
+  const talker = await talkerManager.getAlltalkers();
+  res.status(200).json(talker);
 });
---
+++ src/talkerManager.js
@@ -0,0 +1,32 @@
+const fs = require('fs').promises;
+const { join } = require('path');
+
+const path = './talker.json';
+
+const readTalkerManagerFile = async () => {
+  try {
+    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+    return JSON.parse(contentFile);
+  } catch (error) {
+    return null;
+  }
+};
+
+const writeTalkerManagerFile = async (talkerManager) => {
+  try {
+    await fs.writeFile(join(__dirname, path), JSON.stringify(talkerManager));
+  } catch (error) {
+    return null;
+  }
+};
+
+const getAlltalkers = async () => {
+  const talkerManager = await readTalkerManagerFile();
+  return talkerManager;
+};
+
+module.exports = {
+  getAlltalkers,
+  readTalkerManagerFile,
+  writeTalkerManagerFile,
+};
